204aaefb7ea453ed1e5a9dc370a2f240a70a5209
z530989673 <530989673@qq.com>
2015-09-16 21:23:10 -0700

    First Commit

---
+++ AAAGame/Assets/Main/Entry.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entry : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int i = 0;
+        i = i + 1;
+        Debug.Log("asd");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
